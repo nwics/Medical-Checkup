92778c1c7ff9a48e4e6440722c38c09b84dd5f97
nwics <nabilwicaksono274@gmail.com>
2025-05-22 20:15:10 +0700

    first commit

---
+++ src/main/java/com/medical/medical_chekup/MedicalChekupApplication.java
@@ -0,0 +1,13 @@
+package com.medical.medical_chekup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalChekupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalChekupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medical/medical_chekup/MedicalChekupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.medical_chekup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalChekupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
